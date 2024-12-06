4cbe1416303217bddb36bd704de1a9240faa8212
anusha077 <161424334+anusha077@users.noreply.github.com>
2024-12-06 00:05:08 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,10 +156,7 @@ def process_files(member_outreach_file, event_debrief_file, submitted_file, appr
 def update_from_approved(row):
     try:
         if row['status'] == 'Approved' and row['memberName'] in Approved_Memberships['memberName'].values:
-            match = approved_df.loc[
-                (approved_df['memberName'] == row['memberName']) &
-                (approved_df['status'] == row['status'])
-            ]
+            match = approved_df.loc[(approved_df['memberName'] == row['memberName']) & (approved_df['status'] == row['status'])]
             if not match.empty:
                 row['autoApproved'] = match['autoApproved'].values[0]
                 row['funded'] = match['funded'].values[0] if 'funded' in match.columns else None
